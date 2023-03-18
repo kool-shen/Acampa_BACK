c6648501188698e8d5073efd8b1edb7fde9cbb49
Leo <ferteleo@gmail.com>
2023-03-18 10:42:32 +0100

    routes ok sauf delete

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -11,37 +11,21 @@ cloudinary.config({
   cloud_name: cloud_name,
   api_key: api_key,
   api_secret: api_secret,
+  metadata: true,
 });
 
 /// Photos de la Home ///
+
 router.get("/homepage", function (req, res) {
-  cloudinary.api.resources({ max_results: 500 }).then((data) => {
-    const filteredData = data.resources
-      .filter((item) => {
-        return item.folder.split("/").pop() === "Homepage";
-      })
-      .map((item) => {
-        return {
-          collection: item.folder.split("/").pop(),
-          src: item.url,
-          height: item.height,
-          width: item.width,
-        };
-      });
-    res.json(filteredData);
-    console.log(filteredData);
-  });
-});
-
-/// Photos du shop ///
-
-router.get("/shop", function (req, res) {
-  const expression = `folder:Shop/*`;
+  const expression = `folder:Homepage/* `;
 
   cloudinary.search
     .expression(expression)
     .sort_by("public_id", "desc")
     .max_results(500)
+    .with_field("metadata")
+    .with_field("context")
+
     .execute()
     .then((result) => {
       const filteredData = result.resources.map((item) => {
@@ -50,35 +34,71 @@ router.get("/shop", function (req, res) {
           src: item.secure_url,
           height: item.height,
           width: item.width,
+          metadata: item.metadata,
+          context: item.context,
         };
       });
       res.json(filteredData);
       console.log(filteredData);
     })
-    .catch((error) => console.error(error));
+    .catch((error) => console.error());
 });
 
-/// sous catégories du shop ////
+/// Objets du shop ///
+
+router.get("/shop", function (req, res) {
+  const expression = `folder:Shop/* `;
+
+  cloudinary.search
+    .expression(expression)
+    .sort_by("public_id", "desc")
+    .max_results(500)
+    .with_field("metadata")
+    .with_field("image_metadata")
+    .with_field("context")
+    .execute()
+    .then((result) => {
+      const filteredData = result.resources.map((item) => {
+        return {
+          collection: item.folder.split("/").pop(),
+          src: item.secure_url,
+          height: item.height,
+          width: item.width,
+          metadata: item.metadata,
+          context: item.context,
+        };
+      });
+      const flowers = filteredData.filter(
+        (item) => item.collection === "Fleurs"
+      );
+      const others = filteredData.filter(
+        (item) => item.collection !== "Fleurs"
+      );
+      const sortedData = flowers.concat(others);
+      res.json(sortedData);
+      console.log(sortedData);
+    })
+    .catch((error) => console.error());
+});
+
+/// sous catégories du shop, avec "Fleurs" en premier ////
 
 router.get("/folders", function (req, res) {
   cloudinary.api.sub_folders("Shop", function (error, result) {
     if (error) {
-      console.log(error);
+      console.log();
       res.status(500).send("Erreur lors de la récupération des sous-dossiers");
     } else {
       const folders = result.folders;
-      // Trouver l'objet qui contient "Fleurs" et l'enlever du tableau
       const fleursObj = folders.find((folder) => folder.name === "Fleurs");
       if (fleursObj) {
         folders.splice(folders.indexOf(fleursObj), 1);
       }
-      // Trier le tableau en mettant l'objet qui contient "Fleurs" en premier
       folders.sort((a, b) => {
         if (a.name === "Fleurs") return -1;
         if (b.name === "Fleurs") return 1;
         return 0;
       });
-      // Ajouter l'objet qui contient "Fleurs" au début du tableau si présent
       if (fleursObj) {
         folders.unshift(fleursObj);
       }
@@ -89,27 +109,55 @@ router.get("/folders", function (req, res) {
 
 //// Route test ////
 
-router.get("/", function (req, res) {
+router.get("/test", function (req, res) {
   cloudinary.api.resources_by_asset_folder(
     "Home",
     { metadata: true },
     function (error, result) {
-      console.log(result, error);
+      console.log();
     }
   );
   cloudinary.api
-    .resources({ max_results: 500 })
+    .resources({ max_results: 500, context: true })
     .then((data) => console.log(data));
+});
+
+////
+
+router.get("/get-all-images-metadata", function (req, res) {
+  // Récupération de toutes les ressources d'upload avec les métadonnées structurées
+  cloudinary.api.resources(
+    { max_results: 500, metadata: true },
+    function (error, result) {
+      if (error) {
+        console.log(error);
+      } else {
+        // Construction d'un tableau d'objets contenant les informations des images et leurs métadonnées structurées
+        const imagesWithMetadata = result.resources.map((image) => {
+          return {
+            publicId: image.public_id,
+            format: image.format,
+            width: image.width,
+            height: image.height,
+            metadata: image.metadata,
+          };
+        });
+        console.log(imagesWithMetadata);
+      }
+    }
+  );
 });
 
 /// meta
 router.get("/metadata", function (req, res) {
-  cloudinary.api.list_metadata_fields();
   cloudinary.api
-    .resources({ max_results: 500 })
-    .then((data) => console.log(data));
+    .list_metadata_fields({ context: true })
+    .then((result) => console.log(result));
+  cloudinary.api
+    .resources({ max_results: 500, context: true })
+    .then((data) => console.log());
 });
-
-//cloudinary.v2.api.list_metadata_fields(options).then(callback);
+//cloudinary.v2.api.metadata_field_by_field_id(external_id, options).then(callback);
+//cloudinary.v2.api.metadata_field_by_field_id('in_stock').then(result=>console.log(result));
 
 module.exports = router;
